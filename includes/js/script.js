5225216ac99f6dad9c66408007319f023d76e30e
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2016-09-17 22:54:34 +0300

    finish commit

---
+++ includes/js/script.js
@@ -0,0 +1,21 @@
+/*
+
+My Custom JS
+============
+
+*/
+
+$(function () {
+	$('#alertMe').click(function (e) {
+		e.preventDefault();
+		$('#successAlert').slideDown();
+	});
+
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+
+	$('a.pop').popover();
+
+	$('[rel="tooltip"]').tooltip();
+});
